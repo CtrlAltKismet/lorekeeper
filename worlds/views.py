5233772b3cb9de485e45fc311d0ad897c309f787
CtrlAltKismet <brookesdet@gmail.com>
2026-06-04 17:31:53 +0100

    added about page to urls.py and views.py for navigation

--- worlds/views.py
+++ worlds/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     """Display the Lorekeeper homepage."""
     return render(request, 'worlds/home.html')
+
+def about(request):
+    """Display information about the Lorekeeper application."""
+    return render(request, 'worlds/about.html')
