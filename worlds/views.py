82c88b573d388885499240f91faff6b1233e24e8
CtrlAltKismet <brookesdet@gmail.com>
2026-06-04 17:50:43 +0100

    Added registration to views.py

--- worlds/views.py
+++ worlds/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 def home(request):
@@ -8,3 +11,19 @@ def home(request):
 def about(request):
     """Display information about the Lorekeeper application."""
     return render(request, 'worlds/about.html')
+
+def register(request):
+    """Register a new user account."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully, Welcome to Lorekeeper!')
+            return redirect('home')
+        
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'worlds/register.html', {'form': form})
